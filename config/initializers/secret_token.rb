7a2788d599f74c719e54f70bd8ae727589b8ea0c
balaji.ravi <balaji.ravi@chronus.com>
2016-09-13 11:42:37 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentorApp::Application.config.secret_token = 'f4b0d640c5e710743e7942e4413458ab10f0da13b6ddcf2f1c2dc6cb3e03b2f82ae9d006e2cab94d9e62abf2d9aca96bea65de48ed0456d589d2c58bce2aa7d1'
